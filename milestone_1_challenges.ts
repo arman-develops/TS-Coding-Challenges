57a87636d164b54917122d099f3f9c018426f215
Amon Victor <amon.victor.develops@gmail.com>
2025-06-02 06:51:02 +0300

    Completed Challenge 2: Convert minutes to seconds

--- milestone_1_challenges.ts
+++ milestone_1_challenges.ts
@@ -4,3 +4,10 @@ function addition (num1:number, num2:number): number {
 }
 // console.log(addition(3, 5)); //8
 // console.log(addition(-6, 9)); //3
+
+//challenge 2: convert minutes into seconds
+function convert (minutes:number):number {
+    return minutes * 60;
+}
+// console.log(convert(5)); //300
+// console.log(convert(2)); //120
