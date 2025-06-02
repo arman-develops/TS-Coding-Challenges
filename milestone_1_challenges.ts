598f733913da351a128197c2ae0d709baa18ec44
Amon Victor <amon.victor.develops@gmail.com>
2025-06-02 07:12:15 +0300

    Completed Challenge 12: Multiply by itself

--- milestone_1_challenges.ts
+++ milestone_1_challenges.ts
@@ -141,6 +141,19 @@ function sumEvenNumbers(n:number):number {
     }
     return sum;
 }
-console.log(sumEvenNumbers(6));
-console.log(sumEvenNumbers(10));
-console.log(sumEvenNumbers(5));
+// console.log(sumEvenNumbers(6));
+// console.log(sumEvenNumbers(10));
+// console.log(sumEvenNumbers(5));
+
+//Challenge 12: Multiply by itself
+function powerUp(num:number, times:number):number {
+    let result:number = 1;
+    for(let i = 0; i < times; i++) {
+        result = result * num
+    }
+    return result;
+}
+// console.log(powerUp(2, 3));
+// console.log(powerUp(0));
+// console.log(powerUp(5, 0));
+// console.log(powerUp(3, 4));
