5d689d08b086b40226def519c3bbd2b8be10a004
Amon Victor <amon.victor.develops@gmail.com>
2025-06-02 07:22:07 +0300

    Completed Challenge 2: Find Maximum Value

--- milestone_2_challenges.ts
+++ milestone_2_challenges.ts
@@ -9,3 +9,15 @@ function sumOfPositives(numArray: number[]):number {
     return total;
 }
 // console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+
+// Challenge 2: FInd Maximum Value
+function findMax(numArray: number[]):number {
+    let max:number = 0;
+    for (let i:number = 0; i < numArray.length; i++) {
+        if(numArray[i] > numArray[i + 1]) {
+            max = numArray[i];
+        }
+    }
+    return max;
+}
+// console.log(findMax([3, 7, 2, 9, 5]));
