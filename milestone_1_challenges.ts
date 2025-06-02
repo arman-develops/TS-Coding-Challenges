2bb9769710150e57fed11703084d4aeb7b8ecfdb
Amon Victor <amon.victor.develops@gmail.com>
2025-06-02 06:50:15 +0300

    Completed Challenge 1: Sum of two numbers

---
+++ milestone_1_challenges.ts
@@ -0,0 +1,6 @@
+//challenge 1: sum of two numbers
+function addition (num1:number, num2:number): number {
+    return num1 + num2;
+}
+// console.log(addition(3, 5)); //8
+// console.log(addition(-6, 9)); //3
