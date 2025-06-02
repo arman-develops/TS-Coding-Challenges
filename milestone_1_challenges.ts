fbf7148236a15b1e4c438d6eeeb3888b42ab23f6
Amon Victor <amon.victor.develops@gmail.com>
2025-06-02 06:53:19 +0300

    Completed 4: Check Negative

--- milestone_1_challenges.ts
+++ milestone_1_challenges.ts
@@ -18,3 +18,25 @@ function findPerimeter(length:number, width:number):number {
 }
 // console.log(findPerimeter(6, 7)); //26
 // console.log(findPerimeter(20, 10)); //60
+
+//challenge 4: Check negative
+function isNegative(num:number):boolean {
+    if(num < 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// console.log(isNegative(-23)); //23
+// console.log(isNegative(55)); //55
+
+//challenge 5: Can I drive
+function canDrive(name:string, age:number):void {
+    if(age > 18) {
+        console.log(`${name} is old enough to drive`);
+    } else {
+        console.log(`${name} is not old enough to drive yet`);
+    }
+}
+// canDrive("Jane", 22); //Jane is old enough to drive
+// canDrive("June", 12); //June is not old enough to drive yet
